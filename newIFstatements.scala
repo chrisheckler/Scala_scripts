83fedbf4e3914004ed5876afb5ba6627b9e874aa
chrisheckler <hecklerchris@hotmail.com>
2017-05-21 18:14:58 -0700

    a simple script that implements an if/else statement in a functionally programming style

---
+++ newIFstatements.scala
@@ -0,0 +1,9 @@
+/**
+@created by Heckler
+This is a simple  script that takes the OO way of any regular if/else statement and implements it functionally
+*/
+val a = 15
+val result = if (a < 10) "Less than 10"
+	     else if (a > 10) "Greater than 10"
+             else "It is 10!"
+println(result)	
