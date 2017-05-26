bb27425b9fa5fec6a62b9dbeb0f18f4953ccc43a
chrisheckler <hecklerchris@hotmail.com>
2017-05-25 20:57:27 -0700

    Multiple ways of using interpolation and string formatting

--- scalaInterpolation.scala
+++ scalaInterpolation.scala
@@ -1,2 +1,15 @@
 val a = 99
-println(s"${a} luftballons floating in the summer sky")
+println(s"${a + 3} luftballons floating in the summer sky")
+
+val ticketCost = 50
+val bandName = "Taling Heads"
+println(f"The $bandName%s tickets are probably $$$ticketCost%1.2f")
+
+val percentIncrease = 20
+val musicGenre = "New Wave"
+
+println(f"The $bandName%s tickets are probably $$$ticketCost%1.2f%nThat's a $percentIncrease%% bump because everyone likes $musicGenre")
+
+println(f"""The $bandName%s tickets are probably $$$ticketCost%1.2f
+	|That's a $percentIncrease%% bump because everyone likes $musicGenre""".stripMargin)
+
