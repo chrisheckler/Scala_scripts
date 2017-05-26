e90e1b584b1bd77430ae3530aa97ba8b167beb4e
chrisheckler <hecklerchris@hotmail.com>
2017-05-25 20:31:50 -0700

    interpolation initial

---
+++ scalaInterpolation.scala
@@ -0,0 +1,2 @@
+val a = 99
+println(s"${a} luftballons floating in the summer sky")
--- stringFormat.scala
+++ stringFormat.scala
@@ -1,2 +1,6 @@
 val str = String.format("This is a %s", "Test")
 println(str)
+
+
+val str2 = String.format("This is a %s".format("Test@"))
+println(str2)
