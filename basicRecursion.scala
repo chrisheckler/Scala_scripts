2aa6697936098bfbc79b9c79ddd7270e0c0b2eba
chrisheckler <hecklerchris@hotmail.com>
2017-06-03 18:58:50 -0700

    modified factorial to handle huge numbers

--- basicRecursion.scala
+++ basicRecursion.scala
@@ -1,6 +1,10 @@
-def factorial(n:Int):Int = {
+def factorial(n:BigInt):BigInt = {
    if (n == 0 || n == 1) 1
    else n * factorial(n - 1)
 }
 
 println(factorial(5))
+
+println(factorial(100))
+
+println(factorial(1000))
