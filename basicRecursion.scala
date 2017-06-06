36184a4f61c22c8a8d92c9dff1f407b52c8df11e
chrisheckler <hecklerchris@hotmail.com>
2017-06-05 23:56:51 -0700

    factorial and fact methods combined into one method inside a method

--- basicRecursion.scala
+++ basicRecursion.scala
@@ -1,12 +1,15 @@
 import scala.annotation.tailrec
 
-@tailrec
-def fact(n:BigInt, acc:BigInt):BigInt = {
-   if (n == 0 || n == 1) acc
-   else fact(n - 1, acc * n)
-}
+
 
 def factorial(n:Int) = fact(n, 1)
+  @tailrec
+  def fact(n:BigInt, acc:BigInt):BigInt = {
+    if (n == 0 || n == 1) acc
+    else fact(n - 1, acc * n)
+  }
+  fact(n, 1)
+}
 
 
 
